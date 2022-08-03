abb4842ed8cd1e1452f13965e4c279a4d4c58122
Dhruvam <dhruvamssharma@gmail.com>
2022-08-03 22:37:05 +0530

    init

---
+++ app/src/main/java/com/example/android/trackmysleepquality/CommonConstants.kt
@@ -0,0 +1,11 @@
+package com.example.android.trackmysleepquality
+
+class CommonConstants {
+    companion object {
+        const val TABLE_NAME: String = "daily_sleep_quality_table"
+        const val NIGHT_ID: String = "night_id"
+        const val START_SLEEP_TIME_MILLI: String = "start_sleep_time_milli"
+        const val END_SLEEP_TIME_MILLI: String = "end_sleep_time_milli"
+        const val SLEEP_QUALITY: String = "quality_rating"
+    }
+}
---
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.trackmysleepquality.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.android.trackmysleepquality.CommonConstants
+
+@Dao
+interface SleepDatabaseDao {
+    @Insert
+    fun insert(night: SleepNight)
+
+    @Update
+    fun updateNight(night: SleepNight)
+
+    @Query("SELECT * FROM ${CommonConstants.TABLE_NAME} WHERE ${CommonConstants.NIGHT_ID} = :key")
+    fun get(key: Long): SleepNight?
+
+    @Query("DELETE FROM ${CommonConstants.TABLE_NAME}")
+    fun clear()
+
+    @Query("SELECT * FROM ${CommonConstants.TABLE_NAME} ORDER BY ${CommonConstants.NIGHT_ID} DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
+
+    @Query("SELECT * FROM ${CommonConstants.TABLE_NAME} ORDER BY ${CommonConstants.NIGHT_ID} DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+}
